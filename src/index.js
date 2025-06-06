83bf0df973052b86482598f50a0521ae93937e4f
Rohan Panwar <panwarrohan484@gmail.com>
2025-06-06 21:33:31 +0530

    add logo

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
+
+document.title = "Self Study Library";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
